57d5ee454f056658866503989c7038ae50236950
josiahademesoe <josiahademeso@gmail.comm>
2022-11-04 01:02:23 +0100

    updated the blogpost controller, router

--- controller/blogPost.js
+++ controller/blogPost.js
@@ -1,25 +1,110 @@
-   const { blogPostModel } = require('../model')
-   
-   const createBlog = async (req,res,next)=>{
-        try {
-            const {user:{fullname},body} = req
-            body.author = fullname
-            console.log(body);
-            const user = await blogPostModel.create({...body})
-            res.status(200).json({user})
-        } catch (error) {
-            next(error)
-        }
+const { blogPostModel } = require("../model");
+const { UnAuthorisedError, BadRequest } = require("../errors");
+
+const createBlog = async (req, res, next) => {
+  try {
+    const {
+      user: { fullname, user_id },
+      body,
+    } = req;
+    body.author = fullname;
+    body.createdBy = user_id;
+    const blog = await blogPostModel.create({ ...body });
+    res.status(200).json({ status: true, blog });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getPublishedBlogs = async (req, res, next) => {
+  try {
+    const queryObject = {}
+    queryObject.state = 'Published'
+    const blogs = await blogPostModel.find(queryObject);
+    res.status(200).json({ status: true, blogs });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllBlogs = async(req,rees,next)=>{
+    try {
+        const blogs = await blogPostModel.find();
+        res.status(200).json({ status: true, blogs });
+      } catch (error) {
+        next(error);
+      }
+}
+
+const myBlogs = async (req, res, next) => {
+  try {
+    const { user_id } = req.user;
+    const blog = await blogPostModel.find({ createdBy: user_id });
+    if(!blog){
+        throw new UnAuthorisedError(`no Blog post created yet`)
     }
+    res.status(200).json({ status: true, blog });
+  } catch (error) {
+    next(error);
+  }
+};
 
+const singleBlog = async (req, res, next) => {
+  const { id: blogId } = req.params;
+  try {
+    const blog = await blogPostModel.findOne({ _id: blogId });
+    if (!blog) throw new BadRequest(`no blog post with id:${blogId}`);
+    res.status(200).json({ status: true, blog });
+  } catch (error) {
+    next(error);
+  }
+};
 
-    const getBlogs = async(req,res)=>{
-        try {
-            const user = await blogPostModel.find()
-            res.status(200).json({user})
-        } catch (error) {
-            next(error)
-        }
-    }
+const updateBlog = async (req, res, next) => {
+  const {
+    user: { user_id },
+    body,
+  } = req;
+  const { id: blogId } = req.params;
+  try {
+    const blog = await blogPostModel.findOneAndUpdate(
+      { _id: blogId, createdBy: user_id },
+      { ...body },
+      { new: true, runValidators:true }
+    );
+    if (!blog)
+      throw new UnAuthorisedError("you are not unauthorised for this update");
+    res.status(200).json({ status: true, blog });
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = { createBlog , getBlogs}
+const deleteBlog = async (req, res, next) => {
+  const { user_id } = req.user;
+  const { id: blogId } = req.params;
+  try {
+    const blog = await blogPostModel.deleteOne({
+      _id: blogId,
+      createdBy: user_id,
+    });
+    if (!blog)
+      throw new UnAuthorisedError(
+        "you are not unauthorised to delete this blogpost"
+      );
+
+    res.status(200).json({ status: true, blog });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createBlog,
+  singleBlog,
+  getAllBlogs,
+  getPublishedBlogs,
+  updateBlog,
+  myBlogs,
+  deleteBlog,
+};
---
+++ routes/blogRouter.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const generalBlogRouter = express.Router();
+const blogRouter = express.Router();
+const {
+  createBlog,
+  getPublishedBlogs,
+  updateBlog,
+  deleteBlog,
+  singleBlog,
+  getAllBlogs,
+  myBlogs,
+} = require("../controller/blogPost");
+
+// not logged in users routes
+generalBlogRouter.route("/").get(getPublishedBlogs);
+
+generalBlogRouter.route("//:id").get(singleBlog);
+
+//logged in users routes
+
+blogRouter.route('/create').post(createBlog)
+
+blogRouter.route("/published").get(getPublishedBlogs);
+
+blogRouter.route("/published/:id").get(singleBlog).patch(updateBlog).delete(deleteBlog);;
+
+blogRouter.route('/all').get(getAllBlogs)
+
+blogRouter.route("/all/:id").get(singleBlog).patch(updateBlog).delete(deleteBlog);
+
+module.exports = { generalBlogRouter, blogRouter };
