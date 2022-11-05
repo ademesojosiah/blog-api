dbe97193a2959fee869aa8f9dd3cd540602b77a4
ademesojosiah <josiahademeso@gmail.com>
2022-11-05 19:04:24 +0100

    test update

--- test/home.spec.js
+++ test/home.spec.js
@@ -6,7 +6,7 @@ describe('Home Route', () => {
     it('Should return status true', async () => {
         const response = await request(app).get('/').set('content-type', 'application/json')
         expect(response.status).toBe(200)
-        expect(response.body).toEqual({ status: true,message : "wellcome to my blog" })
+        expect(response.body).toEqual({ status: true,message : "welcome to my blog" })
     })
 
     it('Should return error when routed to undefined route', async () => {
