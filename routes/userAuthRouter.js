e71436acd3096dde35b5001383a519a97f5bffeb
ademesojosiah <josiahademeso@gmail.com>
2023-01-08 15:33:46 +0100

    validation update

--- routes/userAuthRouter.js
+++ routes/userAuthRouter.js
@@ -2,10 +2,10 @@ const express = require('express')
 const passport = require('passport')
 const router = express.Router()
 const {signup , login} = require('../controller/auth')
-const {signUpValidationMiddleware} = require('../validation/user.validation')
+const {signUpValidationMiddleware,logInValidationMiddleware} = require('../validation/user.validation')
 
 
 router.route('/signup').post(signUpValidationMiddleware,passport.authenticate('signUp',{session:false}),signup )
-router.route('/login').post(passport.authenticate('login',{session:false}),login)
+router.route('/login').post(logInValidationMiddleware,passport.authenticate('login',{session:false}),login)
 
 module.exports = router
--- validation/user.validation.js
+++ validation/user.validation.js
@@ -11,7 +11,7 @@ const signUpValidator = Joi.object({
 
 const logInValidator = Joi.object({
     email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+        .email(),
     password: Joi.string()
         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
   })
